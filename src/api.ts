990ea47c23deff618570a58aa109df8fc11530a7
crspo_fcm <samriddho.bose14@gmail.com>
2026-09-11 11:30:34 +0530

    Connect frontend to live backend

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,4 @@
-const API_BASE_URL = "http://localhost:3000";
+const API_BASE_URL = "https://coalguard.onrender.com"
 
 export async function testBackend() {
   const response = await fetch(`${API_BASE_URL}/`);
